b6f061d224a9668b9b4165fda1ff54f0cf0a1f3a
Pav Munyphallin <phallinpav77@gmail.com>
2025-09-03 08:41:02 +0700

    state

--- farm-rpg/scenes/characters/player/idle_state.gd
+++ farm-rpg/scenes/characters/player/idle_state.gd
@@ -25,9 +25,17 @@ func _on_next_transitions() -> void:
 	GameInputEvents.movement_input()
 	if GameInputEvents.is_movement_input():
 		transition.emit("Walk")
+		
+	if player.current_tool == DataTypes.Tools.AxeWood && GameInputEvents.use_tool():
+		transition.emit("Chopping")
+	elif player.current_tool == DataTypes.Tools.TillGround && GameInputEvents.use_tool():
+		transition.emit("Tilling")
+	elif player.current_tool == DataTypes.Tools.WaterCrops && GameInputEvents.use_tool():
+		transition.emit("Watering")
 
 
 func _on_enter() -> void:
+	
 	pass
 
 
--- farm-rpg/scenes/characters/player/watering_state.gd
+++ farm-rpg/scenes/characters/player/watering_state.gd
@@ -20,15 +20,15 @@ func _on_next_transitions() -> void:
 func _on_enter() -> void:
 	match player.player_direction:
 		Vector2.UP:
-			animated_sprite_2d.play("chopping_back")
+			animated_sprite_2d.play("watering_back")
 		Vector2.RIGHT:
-			animated_sprite_2d.play("chopping_right")
+			animated_sprite_2d.play("watering_right")
 		Vector2.DOWN:
-			animated_sprite_2d.play("chopping_front")
+			animated_sprite_2d.play("watering_front")
 		Vector2.LEFT:
-			animated_sprite_2d.play("chopping_left")
+			animated_sprite_2d.play("watering_left")
 		_:
-			animated_sprite_2d.play("chopping_front")
+			animated_sprite_2d.play("watering_front")
 
 
 func _on_exit() -> void:
