14c1f2344660d9897e021d8509958891a1bea499
Pav Munyphallin <phallinpav77@gmail.com>
2025-09-04 14:39:50 +0700

    tree

---
+++ farm-rpg/scenes/components/hit_component.gd
@@ -0,0 +1,9 @@
+class_name HitComponent
+extends Area2D
+
+@export var current_tool : DataTypes.Tools = DataTypes.Tools.None
+@export var hit_damage : int = 1
+
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
